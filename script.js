c9234e18bb0ec2d7d62faba98aaa10e44fa320f0
Abhishek Bhat <bhatabhishek126@gmail.com>
2024-07-02 21:49:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Show real Products
+var prodcuts =[
+                {name:"Chair", headline: "Soft Like Bread",price:"50000",image:"sh.com/photo-1598300042247-d088f8ab3a91?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Q2hhaXJzfGVufDB8fDB8fHww"},
+                {name:"Redmi 13C", headline: "Feels like microvowen",price:"15000",image:"https://m.media-amazon.com/images/I/4180tJsogaL._SX300_SY300_QL70_FMwebp_.jpg"},
+                {name:"One Pus Nord", headline: "Just Awsome",price:"25000",image:"https://m.media-amazon.com/images/I/41W0dAnmX9L._SX300_SY300_QL70_FMwebp_.jpg"}
+
+];
+
+
+var popular=[];
+var cluter ="";
+prodcuts.forEach(function(product){
+    cluter+=``
+})
+
+
+
+// On click buttons added to the cart
+// Show real popular prodcuts
